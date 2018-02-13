5064e67633b16850d4545a3a800f1bb009dd0d25
Rohan Thorat <rthorat1@binghamton.edu>
2018-02-13 15:09:53 -0500

    creating and sending customized response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,7 @@
 self.addEventListener('fetch', function(event) {
-    console.log(event.request);
+    event.respondWith(
+        new Response('<strong class="a-winner-is-me"> Hello! We are sending customized response.</strong>',{
+                headers:{'Content-Type': 'text/html'}
+        })
+    );
 });
